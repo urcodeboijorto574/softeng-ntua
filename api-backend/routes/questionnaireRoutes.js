32ec4aafe2b3576567b80c14d1d9869d259a4600
ntua-el19830 <115047054+ntua-el19830@users.noreply.github.com>
2023-01-04 15:30:00 +0200

    Add files via upload

---
+++ api-backend/routes/questionnaireRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const questionnaireController = require('./../controllers/questionnaireController.js');
+
+const router = express.Router();
+
+router
+    .route('/getAllQuestionnaires')
+    .get(questionnaireController.getAllQuestionnaires);
+
+router.route('/:questionnaireID').get(questionnaireController.getQuestionnaire);
+
+module.exports = router;
