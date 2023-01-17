4b905944f1ebf381ad7cf30271e5b9d18b9a2d34
ntua-el18918 <el18918@mail.ntua.gr>
2023-01-18 01:03:51 +0200

    add healthcheck function in adminController.js

--- api-backend/controllers/adminController.js
+++ api-backend/controllers/adminController.js
@@ -1,7 +1,30 @@
+const mongoose = require('mongoose');
+
+/* DB is the database connection string */
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+);
 
 exports.getHealthcheck = (res, req) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'Everything is fine!'
-    });
+    mongoose
+        .connect(DB, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        })
+        .then(
+            () => { /* DB connection check is successful */
+                return res.status(200).json({
+                    status: 'OK',
+                    dbconnection: '<connection string>'
+                });
+            },
+            err => { /* DB connection check failed */
+                return res.status(500).json({
+                    status: 'failed',
+                    dbconnection: '<connection string>'
+                });
+
+            })
 }
