25b7e833b335dced4a6a3584e316b499c5f0ab0e
ntua-el19830 <115047054+ntua-el19830@users.noreply.github.com>
2023-01-20 00:49:14 +0200

    Update adminController.js
    questionnaire_upd implementation v1 with error handling

--- api-backend/controllers/adminController.js
+++ api-backend/controllers/adminController.js
@@ -28,3 +28,42 @@ exports.getHealthcheck = (res, req) => {
 
             })
 }
+
+
+exports.storeOptionsFromQuestion = catchAsync(async (req, res, next) => {
+    for (let i = 0; i < req.body.questions.length; i++) {
+        for (let j = 0; j < req.body.questions[i].options.length; j++) {
+            // σωζω στο option, το qID και το questionnaireID
+            req.body.questions[i].options[j].questionnaireID =
+                req.body.questionnaireID;
+            req.body.questions[i].options[j].qID = req.body.questions[i].qID;
+            var newOption = await Option.create(
+                req.body.questions[i].options[j]
+            );
+            req.body.questions[i].options[j] = newOption._id.toString();
+        }
+    }
+
+    next();
+});
+
+exports.storeQuestionsFromQuestionnaire = catchAsync(async (req, res, next) => {
+    for (let j = 0; j < req.body.questions.length; j++) {
+        // σωζω στο στο πεδιο questionnaireID της ερωτησης, το ID του ερωτηματολογιου
+        req.body.questions[j].questionnaireID = req.body.questionnaireID;
+        var newQuestion = await Question.create(req.body.questions[j]);
+        req.body.questions[j] = newQuestion._id.toString();
+    }
+
+    next();
+});
+
+exports.createQuestionnaire = catchAsync(async (req, res, next) => {
+    const newQuestionnaire = await Questionnaire.create(req.body);
+    res.status(201).json({
+        status: 'OK',
+    });
+    console.log('Questionnaire-end');
+
+    //next();
+});
