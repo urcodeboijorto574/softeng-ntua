3679df8fd278c6c841d156cde80f3f0688d0ffd6
Ioannis G <el18918@mail.ntua.gr>
2023-01-19 20:14:03 +0200

    doanswer works

--- api-backend/controllers/doanswerController.js
+++ api-backend/controllers/doanswerController.js
@@ -5,10 +5,51 @@ const Session = require(`${__dirname}/../models/sessionModel.js`);
 
 exports.doAnswer = async (req, res) => {
     try {
-        const answer = await Answer.create(req.body);
+        console.log('req.params:', req.params);
+
+        const session = await Session
+            .findOne({
+                sessionID: req.params.session
+            });
+
+        console.log('session:', session);
+
+        if (!session) {
+            return res.status(202).json({
+                status: 'invalidSession',
+                msg: 'There\'s no existing session to submit this answer'
+            });
+        }
+
+        const oldAnswer = await Answer
+            .findOne({
+                qID: req.params.questionID,
+                sessionID: req.params.session,
+                questionnaireID: req.params.questionnaireID,
+                optID: req.params.optionID
+            });
+
+        console.log('oldAnswer:', oldAnswer);
+
+        if (oldAnswer)
+            return res.status(202).json({
+                status: 'fail',
+                msg: 'There\'s already a submitted answer for this session'
+            });
+
+        console.log('Came through');
+
+        const answer = await Answer.create({
+            qID: req.params.questionID,
+            optID: req.params.optionID,
+            sessionID: req.params.session,
+            questionnaireID: req.params.questionnaireID,
+            answertext: req.body.answertext
+        });
 
         return res.status(200).json({
-            status: 'success'
+            status: 'success',
+            msg: 'All good'
         });
     } catch (err) {
         return res.status(404).json({
@@ -16,4 +57,5 @@ exports.doAnswer = async (req, res) => {
             msg: err
         });
     }
+    next();
 };
