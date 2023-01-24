44f80047cb9d29f39d042a585aa1ed3608dcb51a
Ioannis G <el18918@mail.ntua.gr>
2023-01-24 03:50:57 +0200

    added getAllQuestionnaires & getAllSessions, progress on doanswer

--- api-backend/routes/questionnaireRoutes.js
+++ api-backend/routes/questionnaireRoutes.js
@@ -1,14 +1,19 @@
-// const express = require('express');
-// const questionnaireController = require('./../controllers/questionnaireController.js');
+const express = require('express');
+const questionnaireController = require(`${__dirname}/../controllers/questionnaireController.js`);
+const sessionController = require(`${__dirname}/../controllers/sessionController.js`);
 
-// const router = express.Router();
+const router = express.Router();
 
-// router
-//     .route('/getAllQuestionnaires')
-//     .get(questionnaireController.getAllQuestionnaires);
+router
+    .route('/getAllQuestionnaires')
+    .get(questionnaireController.getAllQuestionnaires);
+
+router
+    .route('/:questionnaireID/getAllSessions')
+    .get(sessionController.getAllSessions);
 
 router
     .route('/:questionnaireID')
     .get(questionnaireController.getQuestionnaire);
 
-// module.exports = router;
+module.exports = router;
