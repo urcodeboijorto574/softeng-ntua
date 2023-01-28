6037129cd2a470b6138fb6803075c4579a8ab092
Ioannis G <el18918@mail.ntua.gr>
2023-01-28 21:12:37 +0200

    Created import-data/delete-data endpoint
    Fixed the format of the documents in data/ directory and
    enabled both endpoints, so lines that modify the DB are not commented.

--- api-backend/controllers/importController.js
+++ api-backend/controllers/importController.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const mongoose = require('mongoose');
 const Questionnaire = require(`${__dirname}/../models/questionnaireModel.js`);
 const Question = require(`${__dirname}/../models/questionModel.js`);
 const Option = require(`${__dirname}/../models/optionModel.js`);
@@ -16,39 +15,52 @@ exports.importData = async (req, res, next) => {
     const answersInDataFolder = JSON.parse(fs.readFileSync(`${__dirname}/../../data/answers.json`, 'utf-8'));
     const dataFiles = [answersInDataFolder, sessionsInDataFolder, optionsInDataFolder, questionsInDataFolder, questionnairesInDataFolder];
 
-    try {
-        /* Check if there are questionnaires to import */
-        for (let i = 2; i < 5; ++i)
-            if (!dataFiles[i] || dataFiles[i].length === 0)
-                return res.status(402).json({
-                    status: 'failed',
-                    message: 'no data to import'
-                });
-
-        /* A modification must occur, because the files contain the IDs wrong (kinda) */
-        dataFiles.forEach(collection => {
-            collection.forEach(doc => {
-                let idStr = doc._id.$oid;
-                /* With this line the _id of this document can change (in the stage of "tmod[0]='0';") */
-                // let tmod = idStr.split(''); tmod[0] = '0'; idStr = tmod.join('');
-                delete doc._id;
-                const newObjId = mongoose.Types.ObjectId(idStr);
-                doc['_id'] = newObjId;
+    /* Check if there are questionnaires to import */
+    for (let i = 2; i < 5; ++i) {
+        if (!dataFiles[i] || dataFiles[i].length === 0)
+            return res.status(402).json({
+                status: 'failed',
+                message: 'no data to import'
             });
+    }
+
+    /* (Optional) Change the prefix of the _id */
+    const prefix_id = '0';
+    dataFiles.forEach(collection => {
+        collection.forEach(doc => {
+            doc['_id'] = prefix_id.concat(doc['_id'].slice(prefix_id.length));
+            let docArray;
+            if (doc['questions']) docArray = doc['questions'];
+            else if (doc['options']) docArray = doc['options'];
+            else if (doc['answers']) docArray = doc['answers'];
+            if (docArray)
+                docArray.forEach(el => {
+                    el['_id'] = prefix_id.concat(el['_id'].slice(prefix_id.length));
+                });
         });
+    });
 
+    console.log('Data to import:');
+    dataFiles.forEach(collection => {
+        let doctype;
+        if (collection[0]) {
+            switch (collection[0]) {
+                case dataFiles[0]: doctype = '--------------------------------------------------Answers--------------------------------------------------'; break;
+                case dataFiles[1]: doctype = '--------------------------------------------------Sessions--------------------------------------------------'; break;
+                case dataFiles[2]: doctype = '--------------------------------------------------Options--------------------------------------------------'; break;
+                case dataFiles[3]: doctype = '--------------------------------------------------Questions--------------------------------------------------'; break;
+                default: doctype = '--------------------------------------------------Questionnaires--------------------------------------------------'; break;
+            }
+            console.log(doctype); console.log(collection);
+        }
+    });
 
-        console.log('Data to import:');
-        for (let i = 0; i < 5; ++i)
-            console.log(dataFiles[i]);
-
-        /* Import the data in DB */
+    try {
         process.stdout.write('Start importing data');
         for (let i = 0; i < 5; ++i) {
-            // await Models[i].create(dataFiles[i]);
+            await Models[i].create(dataFiles[i]);
             process.stdout.write(`...${i + 1}/5${i == 4 ? '\n' : ''}`);
         }
-
         const msg = 'Data successfully imported!';
         console.log(msg);
 
@@ -71,16 +83,21 @@ exports.deleteData = async (req, res, next) => {
         console.log('Start deleting data');
 
         for (let i = 0; i < 5; ++i) {
-            // await Models[i].deleteMany();
+            const dataDeleted = await Models[i]
+                .deleteMany()
+                .where('_id')
+                .lte('100000000000000000000000');
+            console.log('Deleted data:', dataDeleted);
             process.stdout.write(`...${i + 1}/5${i == 4 ? '\n' : ''}`);
         }
+
         const msg = 'Data successfully deleted!';
         console.log(msg);
 
         return res.status(402).json({
             status: 'success',
             msg
-        })
+        });
     } catch (err) {
         return res.status(500).json({
             status: 'failed',
