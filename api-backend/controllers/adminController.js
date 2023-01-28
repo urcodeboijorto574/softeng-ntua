eb21f8ec3766d15b97319e6261c5bb04a3582900
Ioannis G <el18918@mail.ntua.gr>
2023-01-29 01:21:39 +0200

    modified adminController.js: enabled resetAll and resetQuestionnaire

--- api-backend/controllers/adminController.js
+++ api-backend/controllers/adminController.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Questionnaire = require(`${__dirname}/../models/questionnaireModel.js`);
 const Question = require(`${__dirname}/../models/questionModel.js`);
-const Session = require('../models/sessionModel');
-const { find } = require('../models/sessionModel');
+const Session = require(`${__dirname}/../models/sessionModel.js`);
 const Option = require(`${__dirname}/../models/optionModel.js`);
 const Answer = require(`${__dirname}/../models/answerModel.js`);
 // const User = require(`${__dirname}/../models/useerModel.js`);
@@ -22,6 +21,7 @@ exports.getHealthcheck = (req, res) => {
             useNewUrlParser: true,
             useCreateIndex: true,
             useFindAndModify: false,
+            useUnifiedTopology: true /* Only for Ioannis' PC */
         })
         .then(
             () => { /* DB connection check is successful */
@@ -33,27 +33,20 @@ exports.getHealthcheck = (req, res) => {
             err => { /* DB connection check failed */
                 return res.status(500).json({
                     status: 'failed',
-                    dbconnection: DB
+                    err
                 });
-
             });
 };
 
-exports.resetAll = async (req, res) => {
+exports.resetAll = async (req, res, next) => {
     try {
         let questionnaires, questions, sessions, options, answers; //, users;
-        // questionnaires = await Questionnaire.find(),
-        // questions = await Question.find(),
-        // sessions = await Session.find(),
-        // options = await Option.find(),
-        // answers = await Answer.find(),
-        // users = await User.find();
-        console.log('questionnaires:', await Questionnaire.find().then(arr => arr.length).catch(err => new Error(err)));
-        console.log('\nquestions', await Question.find().then(arr => arr.length).catch(err => new Error(err)));
-        console.log('\nsessions', await Session.find().then(arr => arr.length).catch(err => new Error(err)));
-        console.log('\noptions', await Option.find().then(arr => arr.length).catch(err => new Error(err)));
-        console.log('\nanswers', await Answer.find().then(arr => arr.length).catch(err => new Error(err)));
-        console.log('\nusers', await User.find().then(arr => arr.length).catch(err => new Error(err)));
+        questionnaires = await Questionnaire.deleteMany();
+        questions = await Question.deleteMany();
+        sessions = await Session.deleteMany();
+        options = await Option.deleteMany();
+        answers = await Answer.deleteMany();
+        // users = await User.deleteMany();
 
         return res.status(402).json({
             status: 'OK'
@@ -64,9 +57,10 @@ exports.resetAll = async (req, res) => {
             reason: err
         });
     }
+    next();
 };
 
-exports.questionnaireUpdate = async (req, res) => {
+exports.questionnaireUpdate = async (req, res, next) => {
     try {
         /* For the line below: need to parse data from multipart/form-data to JSON! */
         const newQuestionnaire = { questionnaireID: 4 };
@@ -116,22 +110,27 @@ exports.questionnaireUpdate = async (req, res) => {
             reason: err
         });
     }
+    next();
 };
 
-exports.resetQuestionnaire = async (req, res) => {
+exports.resetQuestionnaire = async (req, res, next) => {
     try {
         const questionnaire = await Questionnaire.findOne(req.params);
         if (!questionnaire)
             return res.status(400).json({
-                status: 'OK'
+                status: 'failed',
+                reason: 'Invalid questionnaireID'
             });
 
-        const answers = await Answer.find(req.params);
+        const sessions = await Session.find(req.params);
 
-        answers.forEach(async el => {
-            const session = await Session.findOne({ sessionID: el.sessionID });
-            console.log(session.sessionID); // delete session;
-            console.log(el.answertext); // delete el;
+
+        sessions.forEach(async session => {
+            await Answer.deleteMany(session.answers);
+            session.answers.forEach(async ans => {
+                await Answer.deleteMany(req.params);
+            });
+            await Session.deleteOne(session);
         });
 
         return res.status(200).json({
