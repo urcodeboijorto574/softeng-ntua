6e3014fed1e1c6dd0e5976a8b8a692e6f5cdab97
Ioannis G <el18918@mail.ntua.gr>
2023-01-29 18:19:57 +0200

    'Merged' endpoint functions from ioannis-branch
    created answerController.js: created doanswer endpoint,
    created questionnaireController.js: created getallquestionnaires,
        deletequestionnaire, getuserquestionnaire endpoints,
    created sessionController.js: created getallsessions, getallsessionsids,
        getsession endpoints

---
+++ api-backend/controllers/answerController.js
@@ -0,0 +1,109 @@
+const Questionnaire = require(`${__dirname}/../models/questionnaireModel.js`);
+const Question = require(`${__dirname}/../models/questionModel.js`);
+const Session = require(`${__dirname}/../models/sessionModel.js`);
+const Answer = require(`${__dirname}/../models/answerModel.js`);
+const json2csv = require('json2csv');
+
+/**
+ * URL: /intelliq_api/doanswer/:questionnaireID/:questionID/:session/:optionID
+ * 
+ * Κλήση http POST η οποία καταχωρεί την απάντηση optionID η οποία δόθηκε στο γεγονός απάντησης
+ * session στην ερώτηση questionID του ερωτηματολογίου questionnaireID. Δεν επιστρέφει κάποιο
+ * αντικείμενο. Το αναγνωριστικό session είναι μια συμβολοσειρά με 4 τυχαίους χαρακτήρες που
+ * αντιστοιχούν στο γεγονός απάντησης του ερωτηματολογίου (προσοχή: όχι της ερώτησης) από
+ * κάποιον χρήστη.
+ */
+
+exports.doAnswer = async (req, res, next) => {
+    try {
+        /* Check if the questionID is valid */
+        const question = await Question
+            .findOne({ qID: req.params.questionID, questionnaireID: req.params.questionnaireID }, '_id qID questionnaireID options')
+            .populate('options', '-wasChosenBy -opttxt -nextqID -__v');
+        if (!question) {
+            /* Reject the request */
+            return res.status(400).json({
+                status: 'bad request',
+                msg: 'The question specified doesn\'t exist',
+            });
+        }
+
+        /* Check if the optionID is valid */
+        const option = question.options.some(el => el.optID === req.params.optionID);
+        if (!option) {
+            /* Reject the request */
+            return res.status(400).json({
+                status: 'bad request',
+                msg: 'The option specified doesn\'t exist'
+            });
+        }
+
+        /* Check if username is valid */
+        const user = await Session.findOne({ username: req.body.username }, '_id');
+        if (!user) {
+            /* Reject the request */
+            return res.status(400).json({
+                status: 'bad request',
+                msg: 'The user specified doesn\'t exist'
+            })
+        }
+
+        /* Check if a session already exists */
+        const oldSession = await Session
+            .findOne({ sessionID: req.params.session, user: user._id }, '-__v')
+            .populate('answers', '-questionnaireID');
+
+        let currSession = oldSession;
+
+        if (!oldSession) {
+            /* Create new session */
+            let newSession = await Session.create({
+                sessionID: req.params.session,
+                questionnaireID: req.params.questionnaireID,
+                answers: [],
+                user: user._id
+            });
+            currSession = newSession;
+            const questionnaire_id = await Questionnaire
+                .findOne({ questionnaireID: req.params.questionnaireID }, '_id');
+
+            user.questionnaires.push(questionnaire_id);
+        }
+
+        /* Check if there's an old answer submitted to this session */
+        const queryObj = {
+            qID: req.params.questionID,
+            sessionID: req.params.session,
+            optID: req.params.optionID,
+        };
+        const oldAnswer = await Answer.findOne(queryObj).select('-__v');
+        if (oldAnswer) {
+            /* Reject the new answer */
+            return res.status(400).json({
+                status: 'bad request',
+                msg: 'There\'s already a submitted answer for this session',
+                oldAnswer
+            });
+        }
+
+        let newAnswer = queryObj;
+        newAnswer['answertext'] = req.body.answertext, newAnswer['questionnaireID'] = req.params.questionnaireID;
+        ++option['wasChosenBy'];
+
+        const newDoc = await Answer.create(newAnswer);
+        currSession['answers'] = [newDoc._id];
+        await currSession.save();
+        console.log('Answer submitted and sessions\' answers array updated!');
+
+        return res.status(200).json({
+            status: 'success',
+            msg: 'Answer submitted'
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: 'fail',
+            msg: err
+        });
+    }
+    next();
+};
---
+++ api-backend/controllers/questionnaireController.js
@@ -0,0 +1,116 @@
+const Questionnaire = require(`${__dirname}/../models/questionnaireModel.js`);
+const Question = require(`${__dirname}/../models/questionModel.js`);
+const Option = require(`${__dirname}/../models/optionModel.js`);
+const Session = require(`${__dirname}/../models/sessionModel.js`);
+const Answer = require(`${__dirname}/../models/answerModel.js`);
+const User = require(`${__dirname}/../models/userModel.js`);
+const json2csv = require('json2csv');
+
+exports.getAllQuestionnaires = async (req, res, next) => {
+    try {
+        let questionnaires = await Questionnaire
+            .find({}, '-_id')
+            .sort('questionnaireID')
+            .populate({
+                path: 'questions',
+                model: 'Question',
+                select: {
+                    '_id': 0,
+                    '__v': 0,
+                    'wasAnsweredBy': 0,
+                    'questionnaireID': 0
+                },
+                populate: {
+                    path: 'options',
+                    model: 'Option',
+                    select: {
+                        '_id': 0,
+                        '__v': 0
+                    }
+                },
+            });
+
+        return res.status(questionnaires.length !== 0 ? 200 : 402).json({
+            status: questionnaires.length !== 0 ? 'success' : 'no data',
+            data: {
+                questionnaires
+            }
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: 'fail',
+            msg: err
+        });
+    }
+    next();
+};
+
+exports.deleteQuestionnaire = async (req, res, next) => {
+    try {
+        /* Check if given questionnaireID is valid */
+        const theQuestionnaire = await Questionnaire.findOne(req.params, 'questionnaireID _id');
+        if (!theQuestionnaire) {
+            return res.status(400).json({
+                status: 'bad request',
+                msg: `No questionnaire found with questionnaireID ${req.params.questionnaireID}`
+            });
+        }
+
+        /* Delete the questionnaire itself */
+        await Questionnaire.delete(theQuestionnaire);
+
+        /* Delete relevant documents */
+        const questions = await Question.deleteMany(req.params);
+        if (questions)
+            await Option.deleteMany(req.params);
+        const sessions = await Session.deleteMany(req.params);
+        if (sessions)
+            await Answer.deleteMany(req.params);
+
+        /* Show in console the deleted documents */
+        console.log('theQuestionnaire:', theQuestionnaire);
+        console.log('questions:', questions);
+        console.log('options:', options);
+        console.log('sessions:', sessions);
+        console.log('answers:', answers);
+
+        return res.status(402).json({
+            status: 'success',
+            msg: `Everything related with questionnaire ${req.params.questionnaireID} has been deleted`
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: 'fail',
+            msg: err
+        });
+    }
+    next();
+};
+
+exports.getUserQuestionnaires = async (req, res, next) => {
+    try {
+        const user = await User
+            .findOne(req.params)
+            .populate({
+                path: 'questionnaires',
+                model: 'Questionnaire',
+                select: {
+                    '_id': 0
+                },
+                sort: {
+                    'questionnaireID': 1
+                }
+            });
+
+        return res.status(user && user.questionnaires ? 200 : 402).json({
+            status: 'success',
+            data: user.questionnaires
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: 'fail',
+            msg: err
+        });
+    }
+    next();
+};
