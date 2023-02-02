0b1bfef2d726e2535e0225931dd30a9f7b3fb247
ntua-el19053 <el19053@mail.ntua.gr>
2023-02-02 22:54:22 +0200

    first zaf commit

--- api-backend/controllers/questionnaireController.js
+++ api-backend/controllers/questionnaireController.js
@@ -1,108 +1,34 @@
-const Questionnaire = require(`${__dirname}/../models/questionnaireModel.js`);
-const Question = require(`${__dirname}/../models/questionModel.js`);
-const Option = require(`${__dirname}/../models/optionModel.js`);
-const Session = require(`${__dirname}/../models/sessionModel.js`);
-const Answer = require(`${__dirname}/../models/answerModel.js`);
-const User = require(`${__dirname}/../models/userModel.js`);
-const json2csv = require('json2csv');
+const Questionnaire = require('../models/questionnaireModel');
 
-/**
- * Returns all the information about every questionnaire in the data base.
- * @param {JSON} req - JSON object of which no field is used in the function.
- * @param {JSON} res - JSON object that contains a confirmation or a decline of the request.
- * @return {JSON} - The response object created.
- * 
- * URL: {baseURL}/questionnaire/getallquestionnaires
- */
-exports.getAllQuestionnaires = async (req, res, next) => {
+exports.getQuestionnaire = async (req, res) => {
     try {
-        let questionnaires = await Questionnaire
-            .find({}, '-_id')
-            .sort('questionnaireID')
+        const questionnaire = await Questionnaire.findOne({
+            questionnaireID: req.params.questionnaireID,
+        })
+            .select({ _id: 0, __v: 0 })
             .populate({
                 path: 'questions',
-                model: 'Question',
                 select: {
-                    '_id': 0,
-                    '__v': 0,
-                    'wasAnsweredBy': 0,
-                    'questionnaireID': 0
+                    _id: 0,
+                    __v: 0,
+                    wasAnsweredBy: 0,
+                    options: 0,
+                    questionnaireID: 0,
                 },
-                populate: {
-                    path: 'options',
-                    model: 'Option',
-                    select: {
-                        '_id': 0,
-                        '__v': 0
-                    }
-                },
+                options: { sort: { qID: 1 } },
             });
-
-        return res.status(questionnaires.length !== 0 ? 200 : 402).json({
-            status: questionnaires.length !== 0 ? 'success' : 'no data',
-            data: {
-                questionnaires
-            }
-        });
+        if (!questionnaire) {
+            return res.status(400).json({
+                status: 'fail',
+                message: `Questionnaire ID ${req.params.questionnaireID} not found`,
+            });
+        }
+        return res
+            .status(200)
+            .json({ status: 'OK', questionnaire: questionnaire });
     } catch (err) {
-        return res.status(500).json({
-            status: 'fail',
-            msg: err
-        });
+        return res
+            .status(500)
+            .json({ status: 'Fail', message: 'Internal server error' });
     }
-    next();
-};
-
-/**
- * Returns all the questionnaires that a specified user has answered.
- * @param {JSON} req - JSON object that contains the 
- * @param {JSON} res - JSON object that contains the 
- * @param {JSON} next - pointer to the next middleware function
- * @return {JSON} - The response object.
- * 
- * URL: {baseURL}/questionnaire/:questionnaireID
- */
-exports.getUserQuestionnaires = async (req, res, next) => {
-    /* This line is added only for temporary purposes */
-    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
-};
-
-/**
- * Returns all the info about a questionnaire (and its qusetions).
- * @param {JSON} req - JSON object of which req.params contains the questionnaireID.
- * @param {JSON} res - JSOn object that contains the data to send.
- * @return {JSON} - The response object created.
- * 
- * URL: {baseURL}/questionnaire/:questionnaireID/
- */
-exports.getQuestionnaire = async (req, res, next) => {
-    /* This line is added only for temporary purposes */
-    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
-};
-
-
-/**
- * Removes a questionnaire and all related entities from the DB.
- * @param {JSON} req - JSON object that contains the questionnaireID of the to-be-deleted questionnaire.
- * @param {JSON} res - JSON object that contains the data to send.
- * @return {JSON} - The response object created.
- * 
- * URL:  {baseURL}/questionnaire/:questionnaireID
- */
-exports.deleteQuestionnaire = async (req, res, next) => {
-    /* This line is added only for temporary purposes */
-    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
-};
-
-/**
- * Returns all the questionnaires that a user has answered.
- * @param {JSON} req - JSON object that contains the username of the specified user.
- * @param {JSON} res - JSON object that contains the data to send.
- * @return {JSON} - The response object created.
- * 
- * URL: {baseURL}/questionnaire/userquestionnaires/:username
- */
-exports.getUserQuestionnaires = async (req, res, next) => {
-    /* This line is added only for temporary purposes */
-    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
 };
