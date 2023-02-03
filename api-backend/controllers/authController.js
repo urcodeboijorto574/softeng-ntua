ceae60349eb4bc5dd4535dfc2c58b6bcbd859d2f
Dimitris <el19830@mail.ntua.gr>
2023-02-03 02:52:46 +0200

    Finished authController(with csv). Small change in server.js

--- api-backend/controllers/authController.js
+++ api-backend/controllers/authController.js
@@ -6,6 +6,20 @@ const converter = require('json-2-csv');
 const csv = require('csv-express');
 const cookieParser = require('cookie-parser');
 
+// handle responses (send json or csv either)
+const handleResponse = (req, res, statusCode, responseMessage) => {
+    if (req.query.format === 'json' || !req.query.format) {
+        return res.status(statusCode).json(responseMessage);
+    } else if (req.query.format === 'csv') {
+        return res.status(statusCode).csv([responseMessage], true);
+    } else {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'Response format is json or csv!',
+        });
+    }
+};
+
 // error-handling functions
 const handleDuplicateFieldsDB = (err) => {
     const message = 'Username taken! Please provide a new username.';
@@ -23,39 +37,37 @@ const handleValidationErrorDB = (err) => {
 // endpoint to sign up a user
 exports.signup = async (req, res) => {
     try {
+        let responseMessage;
         const newUser = await User.create({
             username: req.body.username,
             role: req.body.usermod,
             password: req.body.password,
         });
-        if (req.query.format === 'json' || !req.query.format) {
-            res.status(200).json({
-                status: 'OK',
-            });
-        }
+        responseMessage = { status: 'OK' };
+        return handleResponse(req, res, 200, responseMessage);
     } catch (err) {
         let error = { ...err };
         if (err.code === 11000) {
             error = handleDuplicateFieldsDB(error);
-        }
-        if (err.name === 'ValidationError') {
+        } else if (err.name === 'ValidationError') {
             error = handleValidationErrorDB(error);
         } else {
-            return res.status(500).json({
+            responseMessage = {
                 status: 'failed',
-                message: 'Internal Server Error',
-            });
+                message: 'Internal Server Error!',
+            };
+
+            return handleResponse(req, res, 500, responseMessage);
         }
-        return res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message,
-        });
+        responseMessage = { status: error.status, message: error.message };
+        return handleResponse(req, res, error.statusCode, responseMessage);
     }
 };
 
 // endpoint to get a user's profile
 exports.getUser = async (req, res) => {
     try {
+        let responseMessage;
         const user = await User.findOne({
             username: req.params.username,
         }).select({
@@ -64,36 +76,55 @@ exports.getUser = async (req, res) => {
             __v: 0,
         });
         if (!user) {
+            responseMessage = {
+                status: 'failed',
+                message: 'No user found with the given username.',
+            };
+
+            return handleResponse(req, res, 402, responseMessage);
+        } else {
+            responseMessage = {
+                status: 'OK',
+                user: user,
+            };
             if (req.query.format === 'json' || !req.query.format) {
-                return res.status(402).json({
+                return res.status(200).json(responseMessage);
+            } else if (req.query.format === 'csv') {
+                return res.status(200).csv(
+                    [
+                        {
+                            status: 'OK',
+                            username: user.username,
+                            role: user.role,
+                        },
+                    ],
+                    true
+                );
+            } else {
+                return res.status(400).json({
                     status: 'failed',
-                    message: 'No user found with the given username.',
+                    message: 'Response format is json or csv!',
                 });
             }
         }
-        if (req.query.format === 'json' || !req.query.format) {
-            res.status(200).json({
-                status: 'OK',
-                user: user,
-            });
-        }
     } catch (err) {
-        return res.status(500).json({
+        responseMessage = {
             status: 'failed',
             message: 'Internal Server Error',
-        });
+        };
+        return handleResponse(req, res, 500, responseMessage);
     }
 };
 
 // endpoint to create a user and update a user's password if user already exists
 exports.createUser = async (req, res) => {
     try {
+        let responseMessage;
         // query if exists a user with the same username and usermod
         let userQuery = await User.findOne({
             username: req.params.username,
             role: req.params.usermod,
         });
-        console.log(userQuery);
         // if no user with the same username and usermod exists, then create a new user
         if (!userQuery) {
             const newUser = await User.create({
@@ -101,67 +132,64 @@ exports.createUser = async (req, res) => {
                 password: req.params.password,
                 role: req.params.usermod,
             });
-            if (req.query.format === 'json' || !req.query.format) {
-                res.status(200).json({
-                    status: 'OK',
-                });
-            }
+            responseMessage = { status: 'OK' };
+            return handleResponse(req, res, 200, responseMessage);
         }
         // else if a user with this username and usermod exists, update the user's password
         else {
-            console.log('here');
             userQuery.password = req.params.password;
             userQuery.passwordChangedAt = new Date();
             await userQuery.save();
-            res.status(200).json({
-                status: 'OK',
-            });
+            responseMessage = { status: 'OK' };
+            return handleResponse(req, res, 200, responseMessage);
         }
     } catch (err) {
         let error = { ...err };
 
         if (err.code === 11000) {
             error = handleDuplicateFieldsDB(error);
-        }
-        if (err.name === 'ValidationError') {
+        } else if (err.name === 'ValidationError') {
             error = handleValidationErrorDB(error);
         } else {
-            return res.status(500).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'Internal Server Error',
-            });
+            };
+            return handleResponse(req, res, 500, responseMessage);
         }
-        return res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message,
-        });
+        responseMessage = { status: error.status, message: error.message };
+        return handleResponse(req, res, error.statusCode, responseMessage);
     }
 };
 
 //endpoint to logout a user
 exports.logout = async (req, res) => {
     try {
+        let responseMessage;
         res.cookie('jwt', 'loggedout', {
             expires: new Date(Date.now() + 10 * 1000),
             httpOnly: true,
             sameSite: 'None',
             secure: true,
         });
-        res.status(200).json({
+        responseMessage = {
             status: 'OK',
             message: 'You are successfully logged out.',
-        });
+        };
+        handleResponse(req, res, 200, responseMessage);
     } catch (err) {
-        return res.status(500).json({
+        responseMessage = {
             status: 'failed',
             message: 'Internal Server Error',
-        });
+        };
+        return handleResponse(req, res, 500, responseMessage);
     }
 };
 
 //endpoint to login a user
 exports.login = async (req, res, next) => {
     try {
+        let responseMessage;
         const username = req.body.username;
 
         //candidate password
@@ -169,15 +197,11 @@ exports.login = async (req, res, next) => {
 
         // 1) Check if username and password exist
         if (!username || !password) {
-            const response = {
+            let responseMessage = {
                 status: 'failed',
                 message: 'Please provide username and password!',
             };
-            if (req.query.format === 'json' || !req.query.format) {
-                return res.status(400).json({
-                    response,
-                });
-            }
+            return handleResponse(req, res, 400, responseMessage);
         }
         // 2) Check if user exists and password is correct
         const user = await User.findOne({
@@ -185,19 +209,19 @@ exports.login = async (req, res, next) => {
         });
 
         if (!user || !(await user.correctPassword(password, user.password))) {
-            if (req.query.format === 'json' || !req.query.format) {
-                return res.status(401).json({
-                    status: 'failed',
-                    message: 'Incorrect username or password',
-                });
-            }
+            responseMessage = {
+                status: 'failed',
+                message: 'Incorrect username or password!',
+            };
+            return handleResponse(req, res, 401, responseMessage);
         }
         if (req.body.usermod) {
             if (user.role != req.body.usermod) {
-                return res.status(401).json({
+                responseMessage = {
                     status: 'failed',
-                    message: 'Incorrect username or password',
-                });
+                    message: 'Incorrect username or password!',
+                };
+                return handleResponse(req, res, 401, responseMessage);
             }
         }
 
@@ -216,14 +240,24 @@ exports.login = async (req, res, next) => {
             sameSize: 'None',
             secure: true,
         });
-        res.status(200).json({
-            token: token,
-        });
+        if (req.query.format === 'json' || !req.query.format) {
+            return res.status(200).json({
+                token: token,
+            });
+        } else if (req.query.format === 'csv') {
+            return res.status(200).csv([{ token: token }], true);
+        } else {
+            return res.status(400).json({
+                status: 'failed',
+                message: 'Response format is json or csv!',
+            });
+        }
     } catch (err) {
-        return res.status(500).json({
+        responseMessage = {
             status: 'failed',
             message: 'Internal Server Error',
-        });
+        };
+        return handleResponse(req, res, 500, responseMessage);
     }
 };
 
@@ -231,6 +265,7 @@ exports.login = async (req, res, next) => {
 // the token is sent with a cookie (http header 'Cookie')
 exports.protect = async (req, res, next) => {
     try {
+        let responseMessage;
         // 1) Getting token and check if it's there
         let token;
         if (req.cookies.jwt) {
@@ -238,10 +273,11 @@ exports.protect = async (req, res, next) => {
         }
 
         if (!token) {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'Please log in to get access.',
-            });
+            };
+            handleResponse(req, res, 401, responseMessage);
         }
         // 2) Verification of the token
         const decoded = await promisify(jwt.verify)(
@@ -253,18 +289,20 @@ exports.protect = async (req, res, next) => {
         // a user logged in, and after a few time the user was deleted. But if someone gets the token he must not have access
         const freshUser = await User.findById(decoded.id);
         if (!freshUser) {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'User no longer exists.',
-            });
+            };
+            handleResponse(req, res, 401, handleResponse);
         }
 
         // 4) Check if user changed password after token was issued
         if (freshUser.changedPasswordAfter(decoded.iat)) {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'User recently changed password. Please log in again.',
-            });
+            };
+            return handleResponse(req, res, 401, handleResponse);
         }
 
         // grant access to protected route
@@ -274,32 +312,37 @@ exports.protect = async (req, res, next) => {
     } catch (err) {
         // errors reference to case 2
         if (err.name === 'JsonWebTokenError') {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'Please log in to get access.',
-            });
+            };
+            return handleResponse(req, res, 401, responseMessage);
         } else if (err.name === 'TokenExpiredError') {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'Token has expired. Please log in again.',
-            });
+            };
+            return handleResponse(req, res, 401, responseMessage);
         } else {
-            return res.status(500).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'Internal Server Error',
-            });
+            };
+            return handleResponse(req, res, 500, responseMessage);
         }
     }
 };
 
 //allow access to routes only for some user category
 exports.restrictTo = (...roles) => {
+    let responseMessage;
     return (req, res, next) => {
         if (!roles.includes(req.userRole)) {
-            return res.status(401).json({
+            responseMessage = {
                 status: 'failed',
                 message: 'User unauthorized to continue!',
-            });
+            };
+            return handleResponse(req, res, 401, responseMessage);
         }
         next();
     };
