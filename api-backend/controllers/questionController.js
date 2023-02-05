d14b8623a62f24ab1b0907b22d35e459c48ab702
ntua-el19053 <el19053@mail.ntua.gr>
2023-02-05 16:37:03 +0200

    second zaf commit

--- api-backend/controllers/questionController.js
+++ api-backend/controllers/questionController.js
@@ -1,14 +1,34 @@
-const Question = require(`${__dirname}/../models/questionModel.js`);
+const Question = require('../models/questionModel');
 
-/**
- * Returns all the info about a question (and its options).
- * @param {JSON} req - JSON object of which req.params contains the questionnaireID and questionID.
- * @param {JSON} res - JSOn object that contains the data to send.
- * @return {JSON} - The response object created.
- * 
- * URL: {baseURL}/question/:questionnaireID/:questionID
- */
-exports.getQuestion = async (req, res, next) => {
-    /* This line is added only for temporary purposes */
-    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
+exports.getQuestion = async (req, res) => {
+    try {
+        const question = await Question.findOne({
+            questionnaireID: req.params.questionnaireID,
+            questionID: req.params.qID,
+        })
+            .select({ _id: 0, __v: 0, wasAnsweredBy: 0 })
+            .populate({
+                path: 'options',
+                select: {
+                    _id: 0,
+                    wasChosenBy: 0,
+                    qID: 0,
+                    questionnaireID: 0,
+                    __v: 0,
+                },
+                options: { sort: { qID: 1 } },
+            });
+        if (!question) {
+            return res.status(400).json({
+                status: 'fail',
+                message: `Question ID ${req.params.qID} not found`,
+            });
+        }
+        return res.status(200).json({ status: 'OK', question: question });
+    } catch (err) {
+        console.log(err);
+        return res
+            .status(500)
+            .json({ status: 'Fail', message: 'Internal server error' });
+    }
 };
