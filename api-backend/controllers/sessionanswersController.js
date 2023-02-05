c83f154d40e9022781411d8f612f815fd40ae756
ntua-el19053 <el19053@mail.ntua.gr>
2023-02-05 16:48:34 +0200

    third commit

---
+++ api-backend/controllers/sessionanswersController.js
@@ -0,0 +1,38 @@
+const session = require('../models/sessionModel');
+
+exports.getSessionanswers = async (req, res) => {
+    try {
+        const sessionanswers = await session
+            .findOne({
+                questionnaireID: req.params.questionnaireID,
+                session: req.params.session,
+            })
+            .select({ _id: 0, __v: 0, submitter: 0 })
+            .populate({
+                path: 'answers',
+                select: {
+                    _id: 0,
+                    optID: 0,
+                    sessionID: 0,
+                    questionnaireID: 0,
+                    __v: 0,
+                },
+                options: { sort: { qID: 1 } },
+            });
+        if (!sessionanswers) {
+            return res
+                .status(400)
+                .json({
+                    status: 'Fail',
+                    message: `Session ID ${req.params.session} not found`,
+                });
+        }
+        return res
+            .status(200)
+            .json({ status: 'OK', sessionanswers: sessionanswers });
+    } catch (err) {
+        return res
+            .status(500)
+            .json({ status: 'Fail', message: 'internal server error' });
+    }
+};
