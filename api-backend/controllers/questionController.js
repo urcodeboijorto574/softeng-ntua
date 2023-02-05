04f9c52c0cbf3f0b56dd1dee4e3a5a71275b70a0
ntua-el19053 <el19053@mail.ntua.gr>
2023-02-05 20:40:29 +0200

    Q ready

--- api-backend/controllers/questionController.js
+++ api-backend/controllers/questionController.js
@@ -1,6 +1,15 @@
-const Question = require('../models/questionModel');
+const Question = require(`${__dirname}/../models/questionModel.js`);
 
-exports.getQuestion = async (req, res) => {
+/**
+ * Returns all the info about a question (and its options).
+ * @param {JSON} req - JSON request object containing the questionnaireID and questionID (req.params).
+ * @param {JSON} res - JSOn response object containing the data to send.
+ * @return {JSON} - The response object created.
+ * 
+ * URL: {baseURL}/question/:questionnaireID/:questionID
+ */
+exports.getQuestion = async (req, res, next) => {
+    /* This line is added only for temporary purposes */
     try {
         const question = await Question.findOne({
             questionnaireID: req.params.questionnaireID,
@@ -20,18 +29,19 @@ exports.getQuestion = async (req, res) => {
             });
         if (!question) {
             return res.status(400).json({
-                status: 'fail',
+                status: 'failed',
                 message: `Question ID ${req.params.qID} not found`,
             });
         }
         if (!req.username === questionnaire.creator) {
-            return res.json({ status: 'Fail', message: 'Access denied' });
+            return res.status.(401).json({ status: 'Failed', message: 'Access denied' });
         }
         return res.status(200).json({ status: 'OK', question: question });
     } catch (err) {
         console.log(err);
         return res
             .status(500)
-            .json({ status: 'Fail', message: 'Internal server error' });
+            .json({ status: 'Failed', message: 'Internal server error' });
     }
+    return res.status('418').json({ status: 'no operation', message: 'I\'m a teapot' });
 };
