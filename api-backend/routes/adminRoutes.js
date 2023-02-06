5b7a545e7bc8e8e5d3efe2489663a6928fff9d0b
ntua-el18642 <115187056+ntua-el18642@users.noreply.github.com>
2023-02-06 21:45:59 +0200

    Update adminRoutes

--- api-backend/routes/adminRoutes.js
+++ api-backend/routes/adminRoutes.js
@@ -1,22 +1,84 @@
 const express = require('express');
+const multer = require('multer');
 const adminController = require(`${__dirname}/../controllers/adminController.js`);
+const authController = require('./../controllers/authController.js');
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(
+        null,
+        './'
+      ); //you tell where to upload the files,
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+
+  const upload = multer({
+    storage: storage,
+    onFileUploadStart: function (file) {
+      console.log(file.originalname + ' is starting ...');
+    },
+  });
+
 router
     .route('/healthcheck')
-    .get(adminController.getHealthcheck);
+    .get(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        adminController.getHealthcheck
+    );
 
 router
     .route('/questionnaire_upd')
-    .post(adminController.questionnaireUpdate);
+    .post(
+        authController.protect,
+        authController.restrictTo('admin'),
+        upload.single('file'),
+        adminController.questionnaireUpdate
+    );
 
 router
     .route('/resetall')
-    .post(adminController.resetAll);
+    .post(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        adminController.resetAll
+    );
 
 router
     .route('/resetq/:questionnaireID')
-    .post(adminController.resetQuestionnaire);
+    .post(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        adminController.resetQuestionnaire
+    );
+
+router
+    .route('/:usermod/:username/:password')
+    .post(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        authController.createUser
+    );
+
+router
+    .route('/users/:username')
+    .get(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        authController.getUser
+    );
+
+router
+    .route('/users/deleteUser/:username')
+    .delete(
+        authController.protect,
+        authController.restrictTo('super-admin'),
+        adminController.deleteUser
+    );
 
 module.exports = router;
