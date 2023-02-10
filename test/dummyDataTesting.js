7cc1bb23e802ef1207ed08db79ce70fc2d67b434
Ioannis G <el18918@mail.ntua.gr>
2023-02-10 22:26:48 +0200

    Completed importData function!

--- test/dummyDataTesting.js
+++ test/dummyDataTesting.js
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 
 
 describe('Import/Export dummy-data endpoints', () => {
-    const supAdmin = {
+    const superAdmin = {
         username: 'TheUltraSuperAdmin',
         password: 'the-password-is-secret',
         usermod: 'super-admin'
@@ -20,7 +20,7 @@ describe('Import/Export dummy-data endpoints', () => {
     describe('Test for importData function', () => {
         describe('Login as super-admin to have access to these endpoints', () => {
             it('it should login as super-admin', (done) => {
-                const req = supAdmin;
+                const req = superAdmin;
                 chai
                     .request(server)
                     .post('/intelliq_api/login')
@@ -39,18 +39,18 @@ describe('Import/Export dummy-data endpoints', () => {
 
         describe('--- Normal case', () => {
             it(`it should respond with status code 200 and a message \'Documents imported successfully.\'`, (done) => {
-                const body = { prefixId: '0012300' };
+                const body = { prefixId: '' };
                 chai
                     .request(server)
                     .post('/intelliq_api/dummy-data/import')
                     .set('Cookie', `jwt=${token}`)
                     .send(body)
                     .end((err, res) => {
-                        res.body.should.have.property('message');
                         res.body.message.should.equal('Documents imported successfully.');
                         res.should.have.status(200);
                         res.body.should.have.property('status');
                         res.body.status.should.equal('OK');
+                        res.body.should.have.property('message');
                         done();
                     })
                     .timeout(1000000);
@@ -58,20 +58,20 @@ describe('Import/Export dummy-data endpoints', () => {
         });
 
         describe('--- Error in given prefixId', () => {
-            describe('prefixId is longer than 23 characters', () => {
-                it('it should respond with code 500 and an error message \'prefixId can\'t be more than 23 characters long\'', (done) => {
-                    const body = { prefixId: '123456789012345678901234' };
+            describe('prefixId is longer than 24 characters', () => {
+                it('it should respond with code 400 and an error message \'prefixId can\'t be more than 24 characters long\'', (done) => {
+                    const body = { prefixId: '1244567890124456789012445' };
                     chai
                         .request(server)
                         .get('/intelliq_api/dummy-data/export')
                         .set('Cookie', `jwt=${token}`)
                         .send(body)
                         .end((err, res) => {
-                            res.should.have.status(500);
+                            res.should.have.status(400);
                             res.body.should.have.property('status');
                             res.body.status.should.equal('failed');
                             res.body.should.have.property('message');
-                            res.body.message.should.equal('prefixId can\'t be more than 23 characters long');
+                            res.body.message.should.equal('prefixId can\'t be more than 24 characters long');
                             done();
                         })
                         .timeout(1000000);
@@ -79,7 +79,7 @@ describe('Import/Export dummy-data endpoints', () => {
             });
 
             describe('prefixId is not a hexademical number', () => {
-                it('it should respond with code 500 and an error message \'prefixId must be a hexademical number\'', (done) => {
+                it('it should respond with code 400 and an error message \'prefixId must be a hexademical number\'', (done) => {
                     let body = { prefixId: 'my_prefixId_is_NaN' };
                     chai
                         .request(server)
@@ -87,7 +87,7 @@ describe('Import/Export dummy-data endpoints', () => {
                         .set('Cookie', `jwt=${token}`)
                         .send(body)
                         .end((err, res) => {
-                            res.should.have.status(500);
+                            res.should.have.status(400);
                             res.body.should.have.property('status');
                             res.body.status.should.equal('failed');
                             res.body.should.have.property('message');
@@ -122,7 +122,7 @@ describe('Import/Export dummy-data endpoints', () => {
     describe('Test for exportData function', () => {
         describe('Login as super-admin to have access to these endpoints', () => {
             it('it should login as super-admin', (done) => {
-                const body = supAdmin;
+                const body = superAdmin;
                 chai
                     .request(server)
                     .post('/intelliq_api/login')
@@ -160,20 +160,20 @@ describe('Import/Export dummy-data endpoints', () => {
         });
 
         describe('--- Error in given prefixId', () => {
-            describe('prefixId is longer than 23 characters', () => {
-                it('it should respond with code 500 and an error message \'prefixId can\'t be more than 23 characters long\'', (done) => {
-                    const body = { prefixId: '123456789012345678901234' };
+            describe('prefixId is longer than 24 characters', () => {
+                it('it should respond with code 400 and an error message \'prefixId can\'t be more than 24 characters long\'', (done) => {
+                    const body = { prefixId: '1244567890124456789012345' };
                     chai
                         .request(server)
                         .get('/intelliq_api/dummy-data/export')
                         .set('Cookie', `jwt=${token}`)
                         .send(body)
                         .end((err, res) => {
-                            res.should.have.status(500);
+                            res.should.have.status(400);
                             res.body.should.have.property('status');
                             res.body.status.should.equal('failed');
                             res.body.should.have.property('message');
-                            res.body.message.should.equal('prefixId can\'t be more than 23 characters long');
+                            res.body.message.should.equal('prefixId can\'t be more than 24 characters long');
                             done();
                         })
                         .timeout(1000000);
@@ -181,7 +181,7 @@ describe('Import/Export dummy-data endpoints', () => {
             });
 
             describe('prefixId is not a hexademical number', () => {
-                it('it should respond with code 500 and an error message \'prefixId must be a hexademical number\'', (done) => {
+                it('it should respond with code 400 and an error message \'prefixId must be a hexademical number\'', (done) => {
                     let body = { prefixId: 'my_prefixId_is_NaN' };
                     chai
                         .request(server)
@@ -189,7 +189,7 @@ describe('Import/Export dummy-data endpoints', () => {
                         .set('Cookie', `jwt=${token}`)
                         .send(body)
                         .end((err, res) => {
-                            res.should.have.status(500);
+                            res.should.have.status(400);
                             res.body.should.have.property('status');
                             res.body.status.should.equal('failed');
                             res.body.should.have.property('message');
