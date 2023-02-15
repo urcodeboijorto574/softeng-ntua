63896d832287f6b612b20723ddf0a274e1e19642
ntua-el20435 <el20435@mail.ntua.gr>
2023-02-16 00:27:13 +0200

    Bug fixed

--- test/APItesting.py
+++ test/APItesting.py
@@ -1,28 +1,28 @@
 import subprocess
 
-result = subprocess.run(['cmd', '/c', 'npm start answerTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start answerTesting.js'], shell = True)
 print(result.stdout)
-# subprocess.run(['C:\\Program Files\\nodejs\\npm', 'start', 'answerTesting.js'], capture_output=True)
-result = subprocess.run(['cmd', '/c', 'npm start authTesting.js'], capture_output=True)
-print(result.stdout)
-
-result = subprocess.run(['cmd', '/c', 'npm start dummyDataTesting.js'], capture_output=True)
+# subprocess.run(['C:\\Program Files\\nodejs\\npm', 'start', 'answerTesting.js'], shell = True)
+result = subprocess.run(['cmd', '/c', 'npm start authTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start questionnaireTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start dummyDataTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start questionnaireUpdateTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start questionnaireTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start sessionTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start questionnaireUpdateTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start useCaseEndpointsTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start sessionTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start userTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start useCaseEndpointsTesting.js'], shell = True)
 print(result.stdout)
 
-result = subprocess.run(['cmd', '/c', 'npm start superAdminTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start userTesting.js'], shell = True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start superAdminTesting.js'], shell = True)
 print(result.stdout)
