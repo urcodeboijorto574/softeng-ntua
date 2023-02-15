eda22db449d57143c49bf79bfa147bfc12c41f03
ntua-el20435 <el20435@mail.ntua.gr>
2023-02-16 00:19:09 +0200

    APItesting progress

--- test/APItesting.py
+++ test/APItesting.py
@@ -1,11 +1,28 @@
 import subprocess
 
-subprocess.run(['npm', 'start', 'answerTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'authTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'dummyDataTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'questionnaireTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'questionnaireUpdateTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'sessionTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'useCaseEndpointsTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'userTesting.js'], capture_output=True)
-subprocess.run(['npm', 'start', 'superAdminTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start answerTesting.js'], capture_output=True)
+print(result.stdout)
+# subprocess.run(['C:\\Program Files\\nodejs\\npm', 'start', 'answerTesting.js'], capture_output=True)
+result = subprocess.run(['cmd', '/c', 'npm start authTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start dummyDataTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start questionnaireTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start questionnaireUpdateTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start sessionTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start useCaseEndpointsTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start userTesting.js'], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(['cmd', '/c', 'npm start superAdminTesting.js'], capture_output=True)
+print(result.stdout)
--- test/cliUnitTesting/unitTest.py
+++ test/cliUnitTesting/unitTest.py
@@ -5,7 +5,6 @@ with open(os.devnull, 'w') as devnull:
     result = subprocess.run(["python", "loginTest.py"], stderr=devnull, check=True)
     result = subprocess.run(["python", "logoutTest.py"], stderr=devnull, check=True)
     result = subprocess.run(["python", "healthcheckTest.py"], stderr=devnull, check=True)
-    # resetall
     result = subprocess.run(["python", "questionnaire_updTest.py"], stderr=devnull, check=True)
     result = subprocess.run(["python", "resetqTest.py"], stderr=devnull, check=True)
     result = subprocess.run(["python", "questionnaireTest.py"], stderr=devnull, check=True)
